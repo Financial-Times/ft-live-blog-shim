6887004504d2804e9f5b62679ae63629429bc1ef
Matt Brennan <mattyb1000@gmail.com>
2016-10-10 13:39:53 +0100

    setting blog meta stuff

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import rawBody from 'raw-body';
 import qs from 'qs';
 import {send} from 'micro';
 import cookie from 'cookie';
+import merge from 'lodash.merge';
 
 const formBody = req => rawBody(req, {encoding: 'utf8'}).then(qs.parse);
 const redirect = (res, location, status = 302) => {
@@ -23,11 +24,45 @@ const setCookies = (res, cookies) => Object.keys(cookies).forEach(
 	key => res.setHeader('set-cookie', cookie.serialize(key, cookies[key]))
 );
 
-const blogForm = (id = '', {title = '', excerpt = ''} = {}) => `<form action="/blog/${id}" method="POST">
+const val = (value, received) => `value="${value}" ${received === value ? 'selected' : ''}`;
+
+const blogForm = (
+	id = '',
+	{authornamestyle = '', contentorder = ''} = {},
+	{title = '', excerpt = '', status = ''} = {}
+) => `<form action="/blog/${id}" method="POST">
 	<input value="${title}" name="title" placeholder="Title">
 	<input value="${excerpt}" name="excerpt" placeholder="Excerpt">
-	<input type="submit" value="Update title">
+	<select name="nameStyle">
+		<option ${val('', authornamestyle)}>Author name style</option>
+		<option ${val('full', authornamestyle)}>Full name</option>
+		<option ${val('initials', authornamestyle)}>Initials</option>
+	</select>
+	<select name="contentOrder">
+		<option ${val('', contentorder)}>Content order</option>
+		<option ${val('descending', contentorder)}>New messages at top</option>
+		<option ${val('ascending', contentorder)}>New messages at bottom</option>
+	</select>
+	<select name="status">
+		<option ${val('', status)}>Blog status</option>
+		<option ${val('pending', status)}>Pending</option>
+		<option ${val('inprogress', status)}>In progress</option>
+		<option ${val('closed', status)}>Archived</option>
+	</select>
+	<input type="submit" value="${id ? 'Update blog' : 'Create blog'}">
 </form>`;
+
+const blogFormMeta = (blog, body) => merge(blog, {
+	config: {
+		authornamestyle: body.nameStyle || 'full',
+		contentorder: body.contentOrder || 'descending',
+	},
+	meta: {
+		title: body.title,
+		excerpt: body.excerpt,
+		status: body.status || 'pending',
+	}
+});
 
 export default route({
 	'/' (req, res) {
@@ -58,20 +93,11 @@ export default route({
 		switch(req.method) {
 			case 'POST': {
 				const id = Math.floor(Math.random() * 0xffffffffff).toString(36);
-				const body = await formBody(req);
 				blogs[id] = {
 					catchup: [],
-					config: {
-						baseurl: `${process.env.NOW_URL || 'http://localhost:3000'}/blog/${id}`,
-						authornamestyle: 'full',
-						contentorder: 'descending',
-					},
-					meta: {
-						title: body.title,
-						excerpt: body.excerpt,
-						status: 'pending',
-					}
 				};
+
+				blogFormMeta(blogs[id], await formBody(req));
 
 				return redirect(res, `/blog/${id}`);
 			}
@@ -83,6 +109,11 @@ export default route({
 		if(!blog) return html(res, `<a href="/">↩</a> blog ${params.id} not found`, 404);
 
 		switch(req.method) {
+			case 'POST': {
+				blogFormMeta(blogs[params.id], await formBody(req));
+				return redirect(res, `/blog/${params.id}`);
+			}
+
 			default: {
 				const {query = {}} = url.parse(req.url, true);
 
@@ -103,7 +134,7 @@ export default route({
 						return html(res, `
 							<a href="/">↩</a>
 							<article>
-							${blogForm(params.id, blog.meta)}
+							${blogForm(params.id, blog.config, blog.meta)}
 
 							<hr>
 							<ul>
