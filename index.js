a91f5b5f215ec41f54277027bb9e1d5ba838ce63
Matt Brennan <mattyb1000@gmail.com>
2016-10-19 14:19:32 +0100

    author color support

--- index.js
+++ index.js
@@ -215,6 +215,7 @@ export default route({
 				authordisplayname: authorName,
 				author: authorName.split(' ').map(p => p[0].toUpperCase()).join(''),
 				authornamestyle: blog.config.authornamestyle,
+				authorcolor: authorName.charCodeAt(0) % 3 + 1
 			},
 		});
 
